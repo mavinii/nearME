72e9ff40e4479e31b38df1f2c5442397a55ae31f
Marcos Oliveira <marcosoliveira@Marcoss-MacBook-Pro.local>
2024-12-26 10:05:20 +0000

    README created

--- src/components/welcome/index.tsx
+++ src/components/welcome/index.tsx
@@ -2,6 +2,18 @@ import { Image, Text, View } from "react-native";
 
 import { s } from "./styles";
 
+/**
+ * The `Welcome` component is a part of the NearMe mobile application.
+ * This React Native component displays a welcome screen with the app's logo,
+ * a title, and a subtitle. The subtitle provides a brief description of the app,
+ * which helps users find the best discounts and promotions in their favorite places nearby.
+ *
+ * @component
+ * @example
+ * return (
+ *   <Welcome />
+ * )
+ */
 export function Welcome() {
     return (
         <View>
@@ -13,8 +25,12 @@ export function Welcome() {
             <Text style={s.title}>Welcome to NearMe!</Text>
             
             <Text style={s.subtitle}>
-                Find discounts and promotions in your {"\n"}
-                favorite places near you.
+                This is an react-native app that helps you {"\n"}
+                to find the best discounts and promotions {"\n"}
+                
+
+                {/* Find discounts and promotions in your {"\n"}
+                favorite places near you. */}
             </Text>
         </View>
     )
